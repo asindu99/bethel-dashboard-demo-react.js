5aa34ed33c210dc55a0e1720d9c4f54c060f2a64
HETTIARACHCHI H.A.D.A.S <asindushashan99@gmail.com>
2023-11-28 11:57:04 +0530

    done some changes

--- src/components/Dashboard-Home.jsx
+++ src/components/Dashboard-Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function DashboardHome() {
   return (
     <div>
-        dsadasdsda
+        
     </div>
   )
 }
--- src/components/Main-Content.jsx
+++ src/components/Main-Content.jsx
@@ -10,10 +10,12 @@ import DashboardProfile from "./Dashboard-Profile";
 function MainContent() {
   return (
     <div className="bg-black h-screen">
+      <div className="right-0 absolute bg-[#aaff00]/100 blur-[300px] bottom-0 w-[50%] h-[200px] rounded-t-[400px] z-[0]">
+      </div>
       <SideBar />
       <Navbar />
 
-      <div className="bg-red-400 mt-14 pl-[280px] w-full">
+      <div className="bg-red-400 mt-14 pl-[280px] w-full z-[100]">
         <Routes>
           <Route path="/dashboard/" element={<DashboardHome />} />
           <Route path="/dashboard/storage" element={<DashboardStorage />} />
