7a052e26498de8e63135b003b0ec6fec9142fa96
Lasari Imalsha <101311493+LasariImalsha@users.noreply.github.com>
2023-11-28 09:14:49 +0530

    updated content

--- src/components/Login_Register/Register.jsx
+++ src/components/Login_Register/Register.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import "../Login_Register/Register.css"
 function Register() {
   return (
-    <section className='h-screen px-5 bg-black'>
+  <section className='h-screen px-5 bg-black'>
       {/* Top vector pattern */}
       <div className='flex flex-col items-center w-full justifyl-center h-[400px] rounded-b-xl  relative bg-black '>
         <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
@@ -92,7 +92,7 @@ function Register() {
       
       </div>
       
-      
+      {/* Footer vector pattern */}
       <div class=" top-[250px] relative ">
           <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 60" preserveAspectRatio="none">
             <path d="M0,0V46.29c47.79,22.2,103.59,32.17,158,28,70.36-5.37,136.33-33.31,206.8-37.5C438.64,32.43,512.34,53.67,583,72.05c69.27,18,138.3,24.88,209.4,13.08,36.15-6,69.85-17.84,104.45-29.34C989.49,25,1113-14.29,1200,52.47V0Z" opacity=".25" class="shape-fill path1"></path>
