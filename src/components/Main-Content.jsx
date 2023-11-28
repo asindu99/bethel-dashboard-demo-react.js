b08f4bad4d754b27dbb1224e3a87c9a5ec1e7ee5
HETTIARACHCHI H.A.D.A.S <asindushashan99@gmail.com>
2023-11-28 16:13:32 +0530

    done some changes

--- src/components/Main-Content.jsx
+++ src/components/Main-Content.jsx
@@ -11,12 +11,10 @@ function MainContent() {
   return (
 
     <div className="bg-black h-screen">
-      {/* <div className="right-0 absolute bg-[#aaff00]/100 blur-[200px] bottom-[50%] w-[200px] h-[200px] rounded-full z-[0]">
-      </div> */}
-      <SideBar />
-      <Navbar />
+        <SideBar />
+        <Navbar />
 
-      <div className=" mt-14 pl-[280px] w-full z-[100]">
+      <div className="pt-24 pl-[280px] w-full z-[100]">
 
         <Routes>
           <Route path="/dashboard/" element={<DashboardHome />} />
--- src/components/Nav-bar.jsx
+++ src/components/Nav-bar.jsx
@@ -1,9 +1,61 @@
-import React from 'react'
+import React, { useState } from 'react'
+import iconUser from "../Images/icons/icon-male-user.png"
+import iconDrop from "../Images/icons/icon-drop.png"
 
 function Navbar() {
+  const [toggleDropDown, setToggleDropDown] = useState(false);
+
+  // toggle drop down
+  const handleToggle = () =>{
+    setToggleDropDown(true);
+  }
+
   return (
-    <div>
-        <h1>this is nav bar</h1>
+    <div className='fixed top-3 pl-[280px] w-full px-2 '>
+      {/* start-main nav bar */}
+        <div className='relative flex backdrop-blur-xl bg-white/10
+         w-full h-[70px] rounded-md text-white items-center px-4 justify-between'>
+
+          {/* left side */}
+          <div className='text-[1.3rem]'>
+            <h3>PLATFORM  </h3>
+          </div>
+
+          {/* right side */}
+          <div className='flex gap-2 text-[1rem] items-center'>
+            {/* img */}
+            <div>
+              <img src={iconUser} alt="" className='w-[25px]'/>
+            </div>
+
+            {/* name and the other */}
+            <div className='flex gap-2'>
+              <h2>First</h2>
+              <h2>Last</h2>
+            </div>
+
+            {/* drop down menu */}
+            <button onClick={handleToggle}>
+              <div>
+                <img src={iconDrop} alt="" className='w-[20px]'/>
+              </div>
+            </button>
+           
+          </div>
+
+        </div>
+      {/* end-nav bar */}
+
+      {/* start-drop down menu */}
+      <div className='absolute w-[200px] backdrop-blur-xl bg-white/10 flex
+       '>
+        {/* email and user name  */}
+        <div className='flex flex-col'>
+          <h3>User Name</h3>
+          <h3>testwith</h3>
+        </div>
+      </div>
+      {/* end- drop down menu */}
     </div>
   )
 }
