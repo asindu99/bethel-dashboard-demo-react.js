9371b27fcfce99ba38c6ec858489529ef6776c85
Lasari Imalsha <101311493+LasariImalsha@users.noreply.github.com>
2023-11-30 14:38:37 +0530

    Updated Content

--- src/components/Login_Register/Register.jsx
+++ src/components/Login_Register/Register.jsx
@@ -134,62 +134,95 @@ function Register() {
         <form onSubmit={handleSubmit} className='px-10 py-5'>
           
       <div className='flex items-center px-3 py-2 border-2 mt-8mb-4 rounded-xl'>
+      <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-white" fill="none"
+          viewBox="0 0 24 24" stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+            d="M12 11c0 3.517-1.009 6.799-2.753 9.571m-3.44-2.04l.054-.09A13.916 13.916 0 008 11a4 4 0 118 0c0 1.017-.07 2.019-.203 3m-2.118 6.844A21.88 21.88 0 0015.171 17m3.839 1.132c.645-2.266.99-4.659.99-7.132A8 8 0 008 4.07M3 15.364c.64-1.319 1-2.8 1-4.364 0-1.457.39-2.823 1.07-4" />
+        </svg>
         <input class="pl-2 outline-none border-none bg-transparent text-white text-[12px] relative"
           type="text"
           name="username"
-          placeholder='username'  
+          placeholder='User Name'  
           autoComplete='off'  
           onChange={handleChange}   
         />
-          {errors.username && <span className='absolute text-sm text-red-600 bottom-[450px] '>{errors.username}</span>}  
+          {errors.username && <span className='absolute text-sm text-red-600 bottom-[460px] '>{errors.username}</span>}  
       </div>
       
       <div className='flex items-center px-3 py-2 mt-8 mb-4 border-2 rounded-xl'>
+      <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-white" viewBox="0 0 20 20"
+        fill="currentColor">
+        <path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
+          clip-rule="evenodd" />
+          </svg>
         <input class="pl-2 outline-none border-none bg-transparent text-white text-[12px] relative"
           type="text"
           name="firstName"
-          placeholder='firstName'  
+          placeholder='First Name'  
           autoComplete='off'  
           onChange={handleChange}   
         />
-          {errors.firstName && <span className='absolute text-sm text-red-600 bottom-[380px] '>{errors.firstName}</span>}  
+          {errors.firstName && <span className='absolute text-sm text-red-600 bottom-[386px] '>{errors.firstName}</span>}  
       </div>
       
       <div className='flex items-center px-3 py-2 mt-8 mb-4 border-2 rounded-xl'>
+      <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-white" viewBox="0 0 20 20"
+        fill="currentColor">
+        <path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
+          clip-rule="evenodd" />
+
+          </svg>
         <input class="pl-2 outline-none border-none bg-transparent text-white text-[12px] relative"
           type="text"
           name="lastName"
-          placeholder='lastName'  
+          placeholder='Last Name'  
           autoComplete='off'  
           onChange={handleChange}   
         />
-          {errors.lastName && <span className='absolute text-sm text-red-600 bottom-[312px] mt-8'>{errors.lastName}</span>}  
+          {errors.lastName && <span className='absolute text-sm text-red-600 bottom-[316px] mt-8'>{errors.lastName}</span>}  
       </div>
       
       <div className='flex items-center px-3 py-2 mt-8 mb-4 border-2 rounded-xl'>
+      <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-white" fill="none"
+            viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+              d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207" />
+      </svg>
         <input  class="pl-2 outline-none border-none bg-transparent text-white text-[12px] relative"
           type="email"
           name="email"
-          placeholder='example@gmail.com'
+          placeholder='Email'
           autoComplete='off'
           onChange={handleChange} 
         />
           {errors.email && <span className='absolute text-sm text-red-600 bottom-[245px]' >{errors.email}</span>}  
       </div>
       <div className='flex items-center px-3 py-2 mt-8 mb-4 border-2 rounded-xl'>
+      <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-white" viewBox="0 0 20 20"
+              fill="currentColor">
+              <path fill-rule="evenodd"
+                d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
+                clip-rule="evenodd" />
+            </svg>
         <input class="pl-2 outline-none border-none bg-transparent text-white text-[12px] relative"
           type="password"
           name="password"
-          placeholder='******'
+          placeholder="Password"
           onChange={handleChange} 
         />
           {errors.password && <span className='absolute text-sm text-red-600 bottom-[175px]'>{errors.password}</span>}  
       </div>
       <div className='flex items-center px-3 py-2 mt-8 mb-4 border-2 rounded-xl'>
+      <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-white" viewBox="0 0 20 20"
+              fill="currentColor">
+              <path fill-rule="evenodd"
+                d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
+                clip-rule="evenodd" />
+      </svg>
         <input class="pl-2 outline-none border-none bg-transparent text-white text-[12px] relative"
           type="password"
           name="confirmPassword"
-          placeholder='******'
+          placeholder='Confirm Password'
           onChange={handleChange} 
         />
           {errors.confirmPassword && <span className='absolute text-sm text-red-600 bottom-[105px]'>{errors.confirmPassword}</span>}  
