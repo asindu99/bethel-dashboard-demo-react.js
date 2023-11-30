33cdeb156b6a1098cbe0e47376be963284c31dc1
Lasari Imalsha <101311493+LasariImalsha@users.noreply.github.com>
2023-11-30 14:29:17 +0530

    updated content

--- src/components/Login_Register/Register.jsx
+++ src/components/Login_Register/Register.jsx
@@ -10,51 +10,95 @@ function Register() {
     
     // const {register,formState:{ errors}} = useForm();  
   
-    const [firstName , setFirstName] = useState('');
-    const [lastName , setlastName] = useState('');
-    const [email , setemail] = useState('');
-    const [password , setPassword] = useState('');
-    const [userName , setUserName] = useState('');
-
-    
-    const [isLoading , setIsLoading] = useState(false)
-
-    const Navigate = useNavigate();
-
-    const handleSubmit = async (e) =>{
-      e.preventDefault();
-      setIsLoading(true)
-
-      const res = await axios.post('https://mw.bethel.network/auth/register', {
-                    email: email,
-                    username: userName,
-                    password: password,
-                    firstName : firstName,
-                    lastName : lastName,
-                }, 
-                {
-                    withCredentials: true
-                });
-                console.log(email,password,userName,firstName,lastName)
-      try {
-        res.status === 200 ? Navigate('/') : setIsLoading(false)
-      } catch (error) {
-        setIsLoading(false)
-      }
+    // const [firstName , setFirstName] = useState('');
+    // const [lastName , setlastName] = useState('');
+    // const [email , setemail] = useState('');
+    // const [password , setPassword] = useState('');
+    // const [userName , setUserName] = useState('');
+    const [formData, setFormData] = useState({
+      username: '',
+      email: '',
+      password: '',
+      confirmPassword: '',
+      firstName:'',
+      lastName:''
       
+    })
+  
+    const [errors, setErrors] = useState({})
+  
+    const handleChange = (e) => {
+      const {name, value} = e.target;
+      setFormData({
+          ...formData, [name] : value
+      })
+    }
+  
+    const handleSubmit = (e) => {
+      e.preventDefault()
       const validationErrors = {}
-      if(!email.email.trim()){
-        validationErrors.email = "Email is required!"
+      if(!formData.username.trim()) {
+          validationErrors.username = "username is required"
       }
       
+      if(!formData.firstName.trim()) {
+        validationErrors.firstName = "FirstName is required"
     }
     
-    const handlechange = (e) =>{
-      const {email,value} = e.target;
-      setemail({email,[email]:value});
-      setemail(e.target.value)
-    }
+    if(!formData.lastName.trim()) {
+      validationErrors.lastName = "LastName is required"
+  }
+  
+      if(!formData.email.trim()) {
+          validationErrors.email = "email is required"
+      } else if(!/\S+@\S+\.\S+/.test(formData.email)){
+          validationErrors.email = "email is not valid"
+      }
+  
+      if(!formData.password.trim()) {
+          validationErrors.password = "password is required"
+      } else if(formData.password.length < 6){
+          validationErrors.password = "password should be at least 6 char"
+      }
+  
+      if(formData.confirmPassword !== formData.password) {
+          validationErrors.confirmPassword = "password not matched"
+      }
+  
+      setErrors(validationErrors)
+  
+      if(Object.keys(validationErrors).length === 0) {
+          alert("Form Submitted successfully")
+      }
     
+    // const [isLoading , setIsLoading] = useState(false)
+
+    // const Navigate = useNavigate();
+
+    
+      // const res = await axios.post('https://mw.bethel.network/auth/register', {
+      //               email: email,
+      //               username: userName,
+      //               password: password,
+      //               firstName : firstName,
+      //               lastName : lastName,
+      //           }, 
+      //           {
+      //               withCredentials: true
+      //           });
+      //           console.log(email,password,userName,firstName,lastName)
+      // try {
+      //   res.status === 200 ? Navigate('/') : setIsLoading(false)
+      // } catch (error) {
+      //   setIsLoading(false)
+      // }
+      
+      
+      
+
+    }
+
+
   return (
   <section className='relative w-full min-h-screen px-5 bg-black'>
       {/* Top vector pattern */}
@@ -77,7 +121,7 @@ function Register() {
       {/* Middle content section */}
       <div className='flex items-center justify-center bg-transparent'>
         {/*Form Card  */}
-      <div className='w-[400px] h-[520px] bottom-16 absolute backdrop-blur-md bg-white/10 rounded-md items-center justify-center'>
+      <div className='w-[400px]  bottom-8 absolute backdrop-blur-md bg-white/10 rounded-md items-center justify-center'>
         {/* Heading section */}
         <div className='relative'>
       <h1 className='text-center text-[18px] text-white pt-4 uppercase'>Register</h1>
@@ -87,85 +131,74 @@ function Register() {
       </div>
         </div>
         {/* Form section */}
-        <form class="px-10 py-5" onSubmit={handleSubmit}>
-       
-        <div class="flex items-center border-2 py-2 px-3 rounded-xl mb-4">
-
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-white" fill="none"
-            viewBox="0 0 24 24" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-              d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207" />
-          </svg>
-          <input onChange={handlechange} class="pl-2 outline-none border-none bg-transparent text-white text-[12px]" type="email" name="" id="" placeholder="Email Address"  />
-            {/* {errors.a && (<small className='text-red-700'>{errors.email.message}</small>)} */}
-        </div>        
-
-        <div class="flex items-center border-2 py-2 px-3 rounded-xl mb-4">
-
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-white" fill="none"
-          viewBox="0 0 24 24" stroke="currentColor">
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-            d="M12 11c0 3.517-1.009 6.799-2.753 9.571m-3.44-2.04l.054-.09A13.916 13.916 0 008 11a4 4 0 118 0c0 1.017-.07 2.019-.203 3m-2.118 6.844A21.88 21.88 0 0015.171 17m3.839 1.132c.645-2.266.99-4.659.99-7.132A8 8 0 008 4.07M3 15.364c.64-1.319 1-2.8 1-4.364 0-1.457.39-2.823 1.07-4" />
-          </svg>
-          <input onChange={(e) =>{setUserName(e.target.value)}} class="pl-2 outline-none border-none bg-transparent text-white text-[12px]" type="text" name="" id="" placeholder="Username"/>
-                {/* {errors.a && (<small className='text-red-700'>{errors.userName.message}</small>)} */}
-
-        </div>
-
-        <div class="flex items-center border-2 py-2 px-3 rounded-xl mb-4">
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-white" viewBox="0 0 20 20"
-        fill="currentColor">
-        <path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
-          clip-rule="evenodd" />
-
-          </svg>
-          <input onChange={(e) =>{setFirstName(e.target.value)}} class="pl-2 outline-none border-none bg-transparent text-white text-[12px]"  type="text" name="" id="" placeholder="First name" />
-                    {/* {errors.a && (<small className='text-red-700'>{errors.firstName.message}</small>)} */}
-
-        </div>
-        
-        <div class="flex items-center border-2 py-2 px-3 rounded-xl mb-4">
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-white" viewBox="0 0 20 20"
-        fill="currentColor">
-        <path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
-          clip-rule="evenodd" />
-
-          </svg>
-          <input onChange={(e) =>{setlastName(e.target.value)}} class="pl-2 outline-none border-none bg-transparent text-white text-[12px]" type="text" name="" id="" placeholder="Last name"  />
-          {/* {errors.a && (<small className='text-red-700'>{errors.lastName.message}</small>)} */}
-
-        </div>
-        
-        <div class="flex items-center border-2 py-2 px-3 rounded-xl mb-4">
-
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-white" viewBox="0 0 20 20"
-              fill="currentColor">
-              <path fill-rule="evenodd"
-                d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
-                clip-rule="evenodd" />
-            </svg>
-            <input onChange={(e) =>{setPassword(e.target.value)}} class="pl-2 outline-none border-none bg-transparent  text-white text-[12px]" type="password" name="" id="" placeholder="Password"  />
-              {/* {errors.a && (<small className='text-red-700'>{errors.password.message}</small>)} */}
-
-        </div>
-        
-        <div class="flex items-center border-2 py-2 px-3 rounded-xl mb-4">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-white" viewBox="0 0 20 20"
-              fill="currentColor">
-              <path fill-rule="evenodd"
-                d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
-                clip-rule="evenodd" />
-            </svg>
-            <input class="pl-2 outline-none border-none bg-transparent  text-white text-[12px]" type="password" name="" id="" placeholder="Confirm Password" />
-        </div>
-
-        <button onClick={handleSubmit} type="submit" class="block w-full bg-[#aaff00]/80 mt-4 py-2 rounded-xl text-white font-semibold mb-2 uppercase">
-        { !isLoading && <h3>Sign Up</h3>} { isLoading && <div className='flex justify-center w-full'><img src={loaderGif} alt='' className='flex w-[100px] py-1 justify-center' /></div>}
-
-        </button>
-        <span class="text-sm ml-2  text-white">Already have an account ?</span>
+        <form onSubmit={handleSubmit} className='px-10 py-5'>
+          
+      <div className='flex items-center px-3 py-2 border-2 mt-8mb-4 rounded-xl'>
+        <input class="pl-2 outline-none border-none bg-transparent text-white text-[12px] relative"
+          type="text"
+          name="username"
+          placeholder='username'  
+          autoComplete='off'  
+          onChange={handleChange}   
+        />
+          {errors.username && <span className='absolute text-sm text-red-600 bottom-[450px] '>{errors.username}</span>}  
+      </div>
+      
+      <div className='flex items-center px-3 py-2 mt-8 mb-4 border-2 rounded-xl'>
+        <input class="pl-2 outline-none border-none bg-transparent text-white text-[12px] relative"
+          type="text"
+          name="firstName"
+          placeholder='firstName'  
+          autoComplete='off'  
+          onChange={handleChange}   
+        />
+          {errors.firstName && <span className='absolute text-sm text-red-600 bottom-[380px] '>{errors.firstName}</span>}  
+      </div>
+      
+      <div className='flex items-center px-3 py-2 mt-8 mb-4 border-2 rounded-xl'>
+        <input class="pl-2 outline-none border-none bg-transparent text-white text-[12px] relative"
+          type="text"
+          name="lastName"
+          placeholder='lastName'  
+          autoComplete='off'  
+          onChange={handleChange}   
+        />
+          {errors.lastName && <span className='absolute text-sm text-red-600 bottom-[312px] mt-8'>{errors.lastName}</span>}  
+      </div>
+      
+      <div className='flex items-center px-3 py-2 mt-8 mb-4 border-2 rounded-xl'>
+        <input  class="pl-2 outline-none border-none bg-transparent text-white text-[12px] relative"
+          type="email"
+          name="email"
+          placeholder='example@gmail.com'
+          autoComplete='off'
+          onChange={handleChange} 
+        />
+          {errors.email && <span className='absolute text-sm text-red-600 bottom-[245px]' >{errors.email}</span>}  
+      </div>
+      <div className='flex items-center px-3 py-2 mt-8 mb-4 border-2 rounded-xl'>
+        <input class="pl-2 outline-none border-none bg-transparent text-white text-[12px] relative"
+          type="password"
+          name="password"
+          placeholder='******'
+          onChange={handleChange} 
+        />
+          {errors.password && <span className='absolute text-sm text-red-600 bottom-[175px]'>{errors.password}</span>}  
+      </div>
+      <div className='flex items-center px-3 py-2 mt-8 mb-4 border-2 rounded-xl'>
+        <input class="pl-2 outline-none border-none bg-transparent text-white text-[12px] relative"
+          type="password"
+          name="confirmPassword"
+          placeholder='******'
+          onChange={handleChange} 
+        />
+          {errors.confirmPassword && <span className='absolute text-sm text-red-600 bottom-[105px]'>{errors.confirmPassword}</span>}  
+      </div>
+      <button type="submit" class="block w-full bg-[#aaff00]/80  py-2 rounded-xl text-white font-semibold mb-2 uppercase mt-8">Submit</button>
+      <span class="text-sm ml-2  text-white">Already have an account ?</span>
         <Link to="/"><span class="text-[#aaff00] cursor-pointer"> Login</span></Link>
-        </form>  
+     
+        </form>
             
       </div> 
       
