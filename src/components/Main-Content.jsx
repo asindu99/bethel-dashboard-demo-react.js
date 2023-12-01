e060d0f3ec7b7383c709aa3b389563d6a595e233
HETTIARACHCHI H.A.D.A.S <asindushashan99@gmail.com>
2023-12-01 12:42:34 +0530

    done changes

--- src/components/Main-Content.jsx
+++ src/components/Main-Content.jsx
@@ -7,11 +7,20 @@ import DashboardProfile from "./Dashboard-Profile";
 import DashboardStorageFolder from "./Dashboard-Storage_Folder";
 import { useDispatch } from "react-redux";
 import toggleSidebarSlice from "../reducers/toggleSidebar";
+import { useEffect, useState } from "react";
+import "../components/style.css"
 
 
 
 function MainContent() {
+  const [isLoading , setIsLoading] = useState(true)
 
+  useEffect(()=>{
+
+    setTimeout(() => {
+      setIsLoading(false)
+    }, 3000);
+  },[])
 
   
   return (
@@ -28,15 +37,29 @@ function MainContent() {
       </div>
       
       {/* middle routing  */}
-      
-      <div  className="relative pt-24 lg:pl-[270px] lg:md-[270px] sm:pl-[10px] lg:px-0 md:px-0 sm:px-4 min-[320px]:px-4 w-full z-[10]">
+      { isLoading && <div className="relative pt-24 lg:pl-[270px] text-white lg:md-[270px] sm:pl-[10px] lg:px-0 md:px-0 sm:px-4 min-[320px]:px-4 w-full z-[10]">
+          <div class="loading w-full">
+            <div class="loading-text">
+              <span class="loading-text-words">L</span>
+              <span class="loading-text-words">O</span>
+              <span class="loading-text-words">A</span>
+              <span class="loading-text-words">D</span>
+              <span class="loading-text-words">I</span>
+              <span class="loading-text-words">N</span>
+              <span class="loading-text-words">G</span>
+            </div>
+          </div>
+      </div>}
+
+      { !isLoading && <div  className="relative pt-24 lg:pl-[270px] lg:md-[270px] sm:pl-[10px] lg:px-0 md:px-0 sm:px-4 min-[320px]:px-4 w-full z-[10]">
         <Routes>
           <Route path="/dashboard" element={<DashboardHome />} />
           <Route path="/dashboard/storage" element={<DashboardStorage />} />
           <Route path="/dashboard/profile" element={<DashboardProfile />} />
           <Route path="/dashboard/storage/folder" element={<DashboardStorageFolder />} />
         </Routes>
-      </div>
+      </div>}
+      
       {/* end- middle routing */}
       
     </div>
