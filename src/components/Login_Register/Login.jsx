57ce4dcabaff789b48181e9a394b2c2bd1ca6c1e
Lasari Imalsha <101311493+LasariImalsha@users.noreply.github.com>
2023-12-14 20:11:21 +0530

    updated content

--- src/components/Login_Register/Login.jsx
+++ src/components/Login_Register/Login.jsx
@@ -3,7 +3,10 @@ import walletImg from "../../Images/BG-Images/wallet-screen-img.jpg"
 import bethelLogo from "../../Images/icons/bethellogo.png"
 import { useDispatch } from "react-redux";
 import WalletAddressSlice from "../../reducers/WalletAddressSlice";
+import heroVid from '../../Images/Videos/video.mp4';
+
 const {ethers} = require("ethers");
+
 
 function Login() {
   const Navigate = useNavigate();
@@ -26,39 +29,23 @@ function Login() {
 
   return (
 
- <section className='relative w-full min-h-screen bg-black'>
-      <div className="text-white w-full min-h-screen flex">
-          {/* right side of the div side */}
-          <div className="relative flex flex-1/2 w-full overflow-hidden">
-            <img src={walletImg} alt="" className="w-full absolute"/>
-          </div>
+    <section className='w-full h-[90vh] top-[90px]'>
+    <video className='absolute object-cover w-full h-full -z-10 ' src={heroVid} autoPlay loop muted/>
+    
+    <div className='w-full h-[90%] flex flex-col justify-center items-center text-white px-4 text-center'>
+  
+      <h1 className="text-[60px] font-bold py-2 ">BETHEL TESTNET</h1>
+                
+      <p className='py-2 text-xl'>Connect Your wallet to Start the Adventure !</p>
 
-          {/* left side of  the div */}
-          <div className="flex flex-1/2 w-full">
-            {/* inside of the right side container */}
-            <div className="w-full">
-
-              {/* bethel logo and bethel  */}
-              <div className="w-full justify-center flex items-center gap-2">
-                <img src={bethelLogo} alt="" className="w-[30px]"/>
-                <h1>BETHEL TESTNET</h1>
-              </div>
-
-              {/* main caption of the wallet connect */}
-              <div className="w-full flex justify-center">
-                  <h1>Connect Your wallet to Start the Adventure !</h1>
-              </div>
-
-              {/* connect wallet button*/}
-              <div className="w-full justify-center flex ">
-                 <button onClick={connectWallet} className="p-2 bg-bethel-green/90 rounded-md px-2 ">
-                  Connect Wallet
-                </button>
-              </div>
-            </div>
-          </div>
-      </div>
-    </section>
+    </div>
+    
+    <div className="-mt-48 font-bold text-center">
+      <button onClick={connectWallet} className="p-2 px-4 text-black bg-white rounded-md text-[20px] hover:text-white hover:bg-bethel-green/70 transition-all 1s ease-in-out">Connect Wallet</button>
+      
+    </div>
+    
+  </section>
 
 
   )
